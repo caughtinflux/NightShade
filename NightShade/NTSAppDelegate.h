e9f4ee2bc5000e75159102225680e242d1fb5d7e
caughtinflux <caughtinflux@icloud.com>
2013-04-04 16:47:13 +0530

    First commit, add empty app template

---
+++ NightShade/NTSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTSAppDelegate.h
+//  NightShade
+//
+//  Created by Aditya KD on 04/04/13.
+//  Copyright (c) 2013 ProtoFlux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NightShade/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NightShade
+//
+//  Created by Aditya KD on 04/04/13.
+//  Copyright (c) 2013 ProtoFlux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NTSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NTSAppDelegate class]));
+    }
+}
