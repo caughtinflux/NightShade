38bdac8e708e93dffabb9dff2aafc90983c3a906
caughtinflux <caughtinflux@icloud.com>
2013-04-07 00:28:01 +0530

    Add useless UICollectionViewController subclasses, storyboards

---
+++ NightShade/NTSTilesViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTSTilesViewController.h
+//  NightShade
+//
+//  Created by Aditya KD on 06/04/13.
+//  Copyright (c) 2013 ProtoFlux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTSTilesViewController : UICollectionViewController
+
+@end
---
+++ NightShade/NTSTilesViewController.m
@@ -0,0 +1,38 @@
+//
+//  NTSTilesViewController.m
+//  NightShade
+//
+//  Created by Aditya KD on 06/04/13.
+//  Copyright (c) 2013 ProtoFlux. All rights reserved.
+//
+
+#import "NTSTilesViewController.h"
+
+@interface NTSTilesViewController ()
+
+@end
+
+@implementation NTSTilesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
