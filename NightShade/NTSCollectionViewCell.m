431ccd13cf2746edd3cb7b7ca340ae5f3e0480bf
caughtinflux <caughtinflux@icloud.com>
2013-05-11 16:57:20 +0530

    Add NTSCollectionViewCell class for shadows.
    Lags a *lot* right now.

---
+++ NightShade/NTSCollectionViewCell.m
@@ -0,0 +1,73 @@
+/*
+ NTSCollectionViewCell.m
+ NightShade
+ Created by Aditya KD on 04/04/13.
+ 
+ Copyright 2013 ProtoFlux
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import "NTSCollectionViewCell.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface NTSCollectionViewCell ()
+- (void)_setup;
+@end
+
+
+@implementation NTSCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self _setup];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if ((self = [super initWithCoder:aDecoder])) {
+        [self _setup];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+}
+
+- (void)_setup
+{
+    self.layer.shadowOpacity = 0.8f;
+    self.layer.shadowOffset = CGSizeZero;
+    self.layer.shadowRadius = 2.f;
+    self.layer.masksToBounds = NO;
+    self.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.layer.shouldRasterize = YES;
+    self.layer.rasterizationScale = [UIScreen mainScreen].scale;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
