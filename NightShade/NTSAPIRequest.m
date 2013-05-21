287b275959287f970b9932f9357ccc797f0fa4a4
caughtinflux <caughtinflux@icloud.com>
2013-05-21 23:58:06 +0530

    Parse returned data in a background queue.
     Thanks to @rpetrich for pointing that out.

--- NightShade/NTSAPIRequest.m
+++ NightShade/NTSAPIRequest.m
@@ -29,28 +29,37 @@
 
 + (void)downloadLatestComicWithImage:(BOOL)getImage completion:(NTSCompletionHandler)handler;
 {
-	[self downloadComicWithNumber:@0 getImage:getImage withCompletion:handler];
+	[self downloadComicWithNumber:@0 getImage:getImage completion:handler];
 }
 
-+ (void)downloadComicWithNumber:(NSNumber *)comicNumber getImage:(BOOL)getImage withCompletion:(NTSCompletionHandler)handler;
++ (void)downloadComicWithNumber:(NSNumber *)comicNumber getImage:(BOOL)getImage completion:(NTSCompletionHandler)handler;
 {
     NSAssert((handler != nil), @"A completion handler must be passed into NTSAPIRequest calls!");
     
-	[NSURLConnection sendAsynchronousRequest:[self _URLRequestForComicNumber:comicNumber] queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {		
+	[NSURLConnection sendAsynchronousRequest:[self _URLRequestForComicNumber:comicNumber] queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
 		if (error) {
             NSLog(@"Error downloading comic data. %i : %@", error.code, error.localizedDescription);
             handler(nil, error);
             return;
         }
         
-        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-        NTSComic *comic = [NTSComic comicWithJSONDictionary:dictionary];
-        
-        if (getImage) {
-            [comic downloadImageWithCompletionHandler:^(NSError *imgError) {
-                handler(comic, error);
-            }];
-        }
+        // Since the handler is called on the current queue, create and parse the data received in a background thread.
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            NSError *jsonError;
+            NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+            if (jsonError) {
+                handler(nil, jsonError);
+                return;
+            }
+            
+            NTSComic *comic = [NTSComic comicWithJSONDictionary:dictionary];
+            
+            if (getImage) {
+                [comic downloadImageWithCompletionHandler:^(NSError *imgError) {
+                    handler(comic, imgError);
+                }];
+            }
+        });
 	}];
 }
 
@@ -58,7 +67,7 @@
 {
     NSString *feedURLString = [NSString stringWithFormat:@"http://xkcd.com/%i/info.0.json", [comicNumber integerValue]];
 	NSURL *feedURL = [NSURL URLWithString:([comicNumber isEqualToNumber:@0] ? @"http://xkcd.com/info.0.json" : feedURLString)];
-    return [[NSURLRequest alloc] initWithURL:feedURL];
+    return [NSURLRequest requestWithURL:feedURL];
 }
 
 @end
